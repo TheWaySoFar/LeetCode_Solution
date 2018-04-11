7a60130a508c0e45d33167111ddc1abbff17b9df
gaolee <103664210@qq.com>
2018-04-11 21:23:27 +0800

    start solve the problem on LeetCode

---
+++ two_sum/two_sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        unsort
+    } 
+};
+
+int main(){
+    printf("hello world!");
+}
