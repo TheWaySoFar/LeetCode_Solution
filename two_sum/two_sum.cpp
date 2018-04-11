80da957087886b12bdd029c28606b1754e38cfb3
gaolee <103664210@qq.com>
2018-04-11 22:21:22 +0800

    two sum with c++

--- two_sum/two_sum.cpp
+++ two_sum/two_sum.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
 #include<vector>
-#include<map>
+#include<unordered_map>
+#include<iterator>
 using namespace std;
 
 
 class Solution{
 public:
     vector<int> twoSum(vector<int>& nums, int target){
-        unsort
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            unordered_map<int, int>::iterator it = mp.find(target - nums[i]); 
+            if(it != mp.end()){
+               return vector<int> {it->second, i}; 
+            }
+            mp[nums[i]] = i; 
+        }
     } 
 };
 
 int main(){
-    printf("hello world!");
+    Solution s;
+    int a[4] = {2, 7, 11, 5};
+    vector<int> b(a, a + 4);
+    vector<int> ans = s.twoSum(b, 9);
+    for(auto x : ans){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
